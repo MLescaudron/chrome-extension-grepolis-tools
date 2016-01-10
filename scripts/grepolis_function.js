2b239420fd51d63b351852a67d418f2ffdb8af52
mlescaudron <mathieu.lescaudron@gmail.com>
2016-01-10 11:56:35 +0100

    Resolve bug windows

--- scripts/grepolis_function.js
+++ scripts/grepolis_function.js
@@ -3,6 +3,8 @@
  * @type {number}
  */
 var second = 300;
+var elems = [];
+
 setTimeout(function () {
     $('#extension_install').animate({opacity: 0, display: 'none'}, 200, function () {
         $('#extension_install').remove()
@@ -41,6 +43,8 @@ function readyToRestart() {
 
     var citiesId = [];
     var baseId = window.Game.townId;
+    elems = [];
+    var number = GPWindowMgr.getNextWindowId() + 1;
 
     for (var town in ITowns.towns) {
         var coord = MapTiles.correctCoordsForIsland(WMap.mapData.findTownInChunks(town));
@@ -56,13 +60,29 @@ function readyToRestart() {
             citiesId.push(townFarms);
         });
     }
-
+    var last = 0;
     citiesId.forEach(function (city) {
-        city.farms.forEach(function (farm) {
+        city.farms.forEach(function (farm,i) {
             window.Game.townId = city.id;
-            getResourcesByTownId(farm);
+            getResourcesByTownId(farm,i);
+            if(i - 1 >= 0){
+                var toClose = elems[i - 1];
+                last = toClose;
+                toClose.close();
+            }
         });
     });
+
+    var lastNumber = GPWindowMgr.getNextWindowId();
+    var result = lastNumber - number;
+    for(var t = 0;result > t;t++){
+        if(GPWindowMgr.GetByID(number + t)){
+            GPWindowMgr.GetByID(number + t).close();
+        }
+    }
+    var toClose = elems[last - 1];
+    toClose.close();
+
     window.Game.townId = baseId;
 
     second = 300;
@@ -74,20 +94,18 @@ function readyToRestart() {
  * Get ressources on the townId
  * @param townId
  */
-function getResourcesByTownId(townId) {
-
+function getResourcesByTownId(townId,i) {
     var action = ($('.looting_checkbox .checkbox_new').hasClass('checked')) ? 'double' : 'normal';
     var c = {action: 'claim_info'};
     var nextIndex = (parseInt(GPWindowMgr.getNextWindowId()) + 1);
 
     nextI = 'gpwnd_' + nextIndex;
 
-    GPWindowMgr.Create(GPWindowMgr.TYPE_FARM_TOWN, 'Extension <span class="farm_town_title_postfix">Grepolis Tools</span>', c, townId);
-    $('#' + nextI).parent().parent().css('visibility', 'hidden');
+    elems[i] = GPWindowMgr.Create(GPWindowMgr.TYPE_FARM_TOWN, 'Extension <span class="farm_town_title_postfix">Grepolis Tools</span>', c, townId);
 
     // Get and call
-    var farmTowncity = GPWindowMgr.getWindowById(nextIndex)
-    farmTowncity.call('claimLoad', townId, action, 300, 32, false, 0);
+    var windowGrepolis = elems[i];
+    windowGrepolis.call('claimLoad', townId, action, 300, 0, false, 0);
 }
 
 
